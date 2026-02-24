442dc11864728d16f629b9a8823634dd8fc8559b
fan4cz <fan4cz@gmail.com>
2026-02-24 19:54:12 +0500

    add something

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const container = document.getElementById('fieldWrapper');
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame()
+{
     renderGrid(3);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension)
+{
     container.innerHTML = '';
 
-    for (let i = 0; i < dimension; i++) {
+    for (let i = 0; i < dimension; i++)
+    {
         const row = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
+        for (let j = 0; j < dimension; j++)
+        {
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
             cell.addEventListener('click', () => cellClickHandler(i, j));
@@ -26,8 +30,18 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
+let isZeroTurn = true;
+
+function cellClickHandler(row, col)
+{
     // Пиши код тут
+    if (isZeroTurn)    {
+        renderSymbolInCell(ZERO, row, col);
+        isZeroTurn = false;
+    } else {
+        renderSymbolInCell(CROSS, row, col);
+        isZeroTurn = true;
+    }
     console.log(`Clicked on cell: ${row}, ${col}`);
 
 
@@ -36,31 +50,36 @@ function cellClickHandler (row, col) {
      */
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333')
+{
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col)
+{
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener()
+{
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler()
+{
     console.log('reset!');
 }
 
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin()
+{
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +90,8 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw()
+{
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +104,7 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col)
+{
     findCell(row, col).click();
 }
